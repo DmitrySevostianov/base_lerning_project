c9a21eb59012900d8aff340659d1f11a0eb0b5ba
DmitrySevostianov <dmitry.sevostianov.mephi@yandex.ru>
2018-08-23 18:10:29 +0300

    some small PEP8 fixes

--- learning/urls.py
+++ learning/urls.py
@@ -5,14 +5,8 @@ from . import views
 app_name = 'learning'
 
 urlpatterns = [
-    
     path('', views.AboutView.as_view(), name='about'),
-
     path('courses', views.CourseList.as_view(), name='courses_list'),
-    
-    path('courses/<int:pk>', views.LessonDetail.as_view(), name='lesson_detail'),
-    
+    path('courses/<int:pk>', views.LessonDetail.as_view(), name='lesson_detail'), 
     path('courses/<slug:slug>', views.CourseDetail.as_view(), name='course_detail'),
-    
-
 ]
--- project/settings.py
+++ project/settings.py
@@ -1,6 +1,4 @@
-
 import os
-
 from configurations import Configuration
 
 class Base(Configuration):
@@ -103,7 +101,5 @@ class Base(Configuration):
 class Dev(Base):
     # SECURITY WARNING: keep the secret key used in production secret!
     SECRET_KEY = 'et&++9@(4i&a+6!#r6%8sr&6k@$py)85cnkde+&*x25emu)^*1'
-
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
-
